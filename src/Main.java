60c8c35e5113a5f2ccc11972ec1b0353090d065a
robemaz <azarovm60@gmail.com>
2023-10-21 00:18:09 +0300

    The unfinished parts of the code have been removed.

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String vyraz = scanner.nextLine();
 
-        vyraz = vyraz.replaceAll("sqrt", "#");
 
         char[] operators = new char[vyraz.length()];
         int operatorCount = 0;
 
         for (int i = 0; i < vyraz.length(); i++) {
             char c = vyraz.charAt(i);
-            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '#') {
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
                 operators[operatorCount] = c;
                 operatorCount++;
             }
@@ -22,7 +21,7 @@ public class Main {
         if (operatorCount == 0) {
             System.out.println("Оператор не найден.");
         } else {
-            String[] numbers = vyraz.split("[+\\-*/%#]|\\(|\\)]");
+            String[] numbers = vyraz.split("[+\\-*/]");
             double result = Double.parseDouble(numbers[0]);
 
             for (int i = 0; i < operatorCount; i++) {
@@ -44,23 +43,6 @@ public class Main {
                         }
                     } else {
                         System.out.println("Делить на ноль нельзя.");
-                    }
-                } else if (operator == '%') {
-                    if (nextNumber != 0) {
-                        if (result >= nextNumber) {
-                            result %= nextNumber;
-                        } else {
-                            System.out.println("Найти остаток из деления на большее значение нельзя.");
-                        }
-                    } else {
-                        System.out.println("Найти остаток деления на ноль нельзя.");
-                    }
-                } else if (operator == '#') {
-                    if (result >= 0) {
-                        result = Math.sqrt(result);
-                    } else {
-                        System.out.println("Извлечение корня из отрицательного числа невозможно.");
-                        return;
                     }
                 }
             }
